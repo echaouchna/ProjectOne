dcfa47789da9e7b640c139df141f39d8aa748d7f
Nabil Echaouch <echaouchna@gmail.com>
2016-02-28 02:33:25 +0100

    Add pagination

--- assets/js/controllers/dataController.js
+++ assets/js/controllers/dataController.js
@@ -1,4 +1,4 @@
-projectOneApp.controller('dataController', ['$scope', '$rootScope', 'dataService', function($scope, $rootScope, dataService) {
+projectOneApp.controller('dataController', ['$scope', '$rootScope', 'dataService', '$filter', function($scope, $rootScope, dataService, $filter) {
   $scope.user = undefined;
 
   $scope.data = [];
@@ -11,6 +11,10 @@ projectOneApp.controller('dataController', ['$scope', '$rootScope', 'dataService
     label: 'true'
   }, ];
 
+  $scope.totalItems = 0;
+  $scope.currentPage = 0;
+  $scope.numPerPage = 3;
+
   $scope.update = function(row) {
     dataService.updateRow(row).then(function(response) {
       row.close_date = response.close_date;
@@ -19,6 +23,66 @@ projectOneApp.controller('dataController', ['$scope', '$rootScope', 'dataService
 
   dataService.getIsins().then(function(response) {
     $scope.data = response;
+    $scope.filtered = $filter('filter')($scope.data, $scope.searchObject);
+    $scope.totalItems = $scope.filtered.length;
+    if ($scope.totalItems > 0) {
+      $scope.currentPage = 1;
+    }
   });
+
+  $scope.paginate = function(value) {
+    var begin, end, index;
+    begin = ($scope.currentPage - 1) * $scope.numPerPage;
+    end = begin + $scope.numPerPage;
+    index = $scope.filtered.indexOf(value);
+    return (begin <= index && index < end);
+  };
+
+  $scope.prevPage = function() {
+    if ($scope.currentPage > 1) {
+      $scope.currentPage--;
+    }
+  };
+
+  $scope.nextPage = function() {
+    var numberOfPages = Math.ceil($scope.data.length / $scope.numPerPage);
+    if ($scope.currentPage < numberOfPages) {
+      $scope.currentPage++;
+    }
+  };
+
+  $scope.setPage = function(n) {
+    $scope.currentPage = n;
+  };
+
+  $scope.range = function() {
+    var ret = [];
+    var numberOfPages = Math.ceil($scope.totalItems / $scope.numPerPage);
+    if (numberOfPages < $scope.currentPage) {
+      $scope.currentPage = 1;
+    }
+    var start = $scope.currentPage - 2;
+    var gap = 2;
+    if (start < 1) {
+      gap += 1 - start;
+      start = 1;
+    }
+    if (start + gap > numberOfPages) {
+      gap = numberOfPages - $scope.currentPage;
+    }
+    var end = $scope.currentPage + gap;
+    for (var i = start; i <= end; i++) {
+      ret.push(i);
+    }
+    return ret;
+  };
+
+  $scope.$watch('searchObject', function(newVal, oldVal) {
+    $scope.filtered = $filter('filter')($scope.data, newVal);
+    $scope.totalItems = $scope.filtered.length;
+    if ($scope.totalItems > 0) {
+      $scope.currentPage = 1;
+    }
+  }, true);
 
 }]);
